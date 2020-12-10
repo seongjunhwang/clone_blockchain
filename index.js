839e94437ecf107905686c12f630861ca2ca91b2
seongjunhwang <samkukzi3@gmail.com>
2020-12-10 12:13:53 +0900

    tsc-watch setting

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "okaka", age = 23, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`hello ${name}, you are ${age}, you are ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+    return (`hello ${name}, you are ${age}, you are ${gender}!!`)
+};
+
+console.log(sayHi("okaka", 123, "male"));
+
+
+export {};
