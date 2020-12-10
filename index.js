80e3ba6f636383eee1154cf0cb55ca8ff2ed0eb0
seongjunhwang <samkukzi3@gmail.com>
2020-12-10 11:53:02 +0900

    typescript practice

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "okaka", age = 23, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+const name = "okaka",
+    age = 23,
+    gender = "male";
+
+
+const sayHi = (name, age, gender?) => {
+    console.log(`hello ${name}, you are ${age}, you are ${gender}`)
+};
+
+sayHi(name, age);
+
+
+export {};
